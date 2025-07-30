ec43644b5fef8ce95e3387adaf0364db134433ff
tarun <tarunmedisetti13@gmail.com>
2025-07-30 13:17:39 +0530

    Used Form data

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import { Routes, Route } from 'react-router-dom'
-import Navbar from './components/Navbar'
-import About from './pages/About';
-import Home from './pages/Home';
-import Contact from './pages/Contact';
-import Projects from './pages/Projects'
-import Timer from './components/Timer'
-function App() {
-  return (<>
-    <Navbar />
-    <Routes className='w-full h-[80vh]'>
-      <Route path='/' element={<Home />} />
-      <Route path='/about' element={<About />} />
-      <Route path='/contact' element={<Contact />} />
-      <Route path='/projects' element={<Projects />} />
-    </Routes>
-    <Timer />
-
-  </>);
+import React from 'react'
+import Form from './Form'
+const App = () => {
+  return (
+    <div>
+      <Form />
+    </div>
+  )
 }
 
 export default App
---
+++ src/Form.jsx
@@ -0,0 +1,29 @@
+import React, { useState } from 'react'
+
+const Form = () => {
+    const [formData, setFormData] = useState({
+        name: '',
+        email: ''
+    });
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData({
+            ...formData,
+            [name]: value
+        });
+    };
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        alert(`name:${formData.name}
+            email:${formData.email}`);
+    }
+    return (
+        <form onSubmit={handleSubmit} className='flex flex-col gap-1 items-center '>
+            <input type="text" name='name' value={formData.name} onChange={handleChange} className='border' />
+            <input type="email" name='email' value={formData.email} onChange={handleChange} className='border' />
+            <button type='submit' className='border bg-blue-400 cursor-pointer'>Submit</button>
+        </form>
+    )
+}
+
+export default Form
